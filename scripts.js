60c6fc1dbf1f922890ad2f3775552589b931b6b7
Robert Mayo <rbrtm984@gmail.com>
2023-03-25 12:54:03 -0400

    Add rockPaperScissors function

--- scripts.js
+++ scripts.js
@@ -1,7 +1,56 @@
 function getComputerChoice () {
     let computerChoice = Math.floor(Math.random() * 3);
-    console.log(computerChoice);
+    switch(computerChoice) {
+        case 0:
+            computerChoice = "rock";
+            break;
+        case 1:
+            computerChoice = "paper";
+            break;
+        case 2:
+            computerChoice = "scissors";
+            break;
+    };
+    
     return computerChoice;
 }
 
-getComputerChoice();
+let computerChoice = getComputerChoice();
+
+console.log(computerChoice);
+
+function rockPaperScissors (playerSelection, computerChoice) {
+    playerSelection = playerSelection.toLowerCase();
+    console.log(playerSelection);
+    if (playerSelection === "rock" && computerChoice === "paper") {
+        console.log("it worked");
+        return "Paper beats rock! The computer wins.";
+    } else if (playerSelection === "rock" && computerChoice === "scissors") {
+        console.log("it worked");
+        return "Rock beats scissors! The computer wins.";
+    } else if (playerSelection === "rock" && computerChoice === "rock"){
+        console.log("it worked");
+        return "Rock ties rock! No one wins ...";
+    } else if (playerSelection === "paper" && computerChoice === "paper") {
+        console.log("it worked");
+        return "Paper ties paper! No one wins ...";
+    }  else if (playerSelection === "paper" && computerChoice === "scissors") {
+        console.log("it worked");
+        return "Scissors beats paper! The computer wins.";
+    }  else if (playerSelection === "paper" && computerChoice === "rock") {
+        console.log("it worked");
+        return "Paper beats rock! The player wins.";
+    }  else if (playerSelection === "scissors" && computerChoice === "paper") {
+        console.log("it worked");
+        return "Scissors beats paper! The player wins.";
+    }  else if (playerSelection === "scissors" && computerChoice === "scissors") {
+        console.log("it worked");
+        return "Scissors ties scissors! No one wins ...";
+    }  else if (playerSelection === "scissors" && computerChoice === "rock") {
+        console.log("it worked");
+        return "Rock beats scissors! The computer wins.";
+    };
+    return "Please enter either rock paper or scissors friend";
+};
+
+console.log(rockPaperScissors("Yoda", computerChoice));
